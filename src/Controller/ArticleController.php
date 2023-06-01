2f0dc07b8eb91cf4376aa4e8adeaa4afb732772f
Baptiste <tabarbaptiste@gmail.com>
2023-06-01 23:24:23 +0200

    first commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class ArticleController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/article', name: 'app_article')]
+    public function index(): Response
+    {
+        $articleRepository = $this->entityManager->getRepository(Article::class);
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/article/new', name: 'app_article_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            // Récupérer les données du formulaire
+            $nom = $request->request->get('nom');
+            $price = $request->request->get('price');
+            $quantite = $request->request->get('quantite');
+            $description = $request->request->get('description');
+            $stock = ($quantite >= 1) ? 'oui' : 'non';
+
+            // Créer une nouvelle instance d'Article
+            $article = new Article();
+            $article->setNom($nom);
+            $article->setPrice($price);
+            $article->setQuantite($quantite);
+            $article->setDescription($description);
+            $article->setStock($stock);
+
+            // Enregistrer l'article en base de données
+            $this->entityManager->persist($article);
+            $this->entityManager->flush();
+
+            // Rediriger vers la liste des articles
+            return $this->redirectToRoute('app_article');
+        }
+
+        return $this->render('article/new.html.twig');
+    }
+
+    #[Route('/article/edit/{id}', name: 'app_article_edit', methods: ['GET', 'POST'], requirements: ['id' => '\d+'])]
+    public function edit(Request $request, Article $article): Response
+    {
+        // Création du formulaire de modification
+        $form = $this->createFormBuilder($article)
+            ->add('nom', TextType::class)
+            ->add('price', NumberType::class)
+            ->add('quantite', NumberType::class)
+            ->add('description', TextareaType::class)
+            ->add('stock', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Modifier'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Enregistrer les modifications en base de données
+            $this->entityManager->flush();
+
+            // Rediriger vers la liste des articles
+            return $this->redirectToRoute('app_article');
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'form' => $form->createView(),
+            'article' => $article,
+        ]);
+    }
+
+
+
+    /*public function index(): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }*/
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $stock = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+        
+        // Mettre à jour la valeur du stock en fonction de la quantité
+        $this->stock = ($quantite >= 1) ? 'oui' : 'non';
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStock(): ?string
+    {
+        return $this->stock;
+    }
+
+    public function setStock(string $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
